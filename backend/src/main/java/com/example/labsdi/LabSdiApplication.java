b8a361793a1e2bd5eb1c3da384ecc56238e01c1e
Goth11 <leonardo.ciurcau@stud.ubbcluj.ro>
2023-04-03 21:50:21 +0300

    add jar

--- backend/src/main/java/com/example/labsdi/LabSdiApplication.java
+++ backend/src/main/java/com/example/labsdi/LabSdiApplication.java
@@ -2,9 +2,6 @@ package com.example.labsdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LabSdiApplication {
