25cd453b24cd21b56131961a759f2dfd8d067149
Goth11 <leonardo.ciurcau@stud.ubbcluj.ro>
2023-04-03 21:48:22 +0300

    webconfig add

--- backend/src/main/java/com/example/labsdi/LabSdiApplication.java
+++ backend/src/main/java/com/example/labsdi/LabSdiApplication.java
@@ -12,14 +12,4 @@ public class LabSdiApplication {
     public static void main(String[] args) {
         SpringApplication.run(LabSdiApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("*");
-            }
-        };
-    }
 }
