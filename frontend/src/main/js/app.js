86472fcfb2135a2bd3ea85913f29d5e6e6dc8fe1
Goth11 <leonardo.ciurcau@stud.ubbcluj.ro>
2023-04-06 12:22:49 +0300

    add backend link

--- frontend/src/main/js/app.js
+++ frontend/src/main/js/app.js
@@ -11,7 +11,7 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 class App extends React.Component {
-    static apiString = "http://ec2-13-50-224-132.eu-north-1.compute.amazonaws.com";
+    static apiString = "http://ec2-16-16-90-44.eu-north-1.compute.amazonaws.com";
 
     constructor(props) {
         super(props);
