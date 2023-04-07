77d8fc653a1fa6353e3aa9f532028eb5e296ff13
Goth11 <leonardo.ciurcau@stud.ubbcluj.ro>
2023-04-07 13:44:27 +0300

    Update ClientController.java

--- backend/src/main/java/com/example/labsdi/controller/ClientController.java
+++ backend/src/main/java/com/example/labsdi/controller/ClientController.java
@@ -22,7 +22,7 @@ public class ClientController {
         return clientService.addClient(client);
     }
 
-    @GetMapping("/clients/all")
+    @GetMapping("/clients/all123")
     public List<Client> getAllClients() {
         return clientService.getAllClients();
     }
