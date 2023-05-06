0eca27d7dd928d154745b9c0f89dc96d9aea7a8f
Goth11 <leonardo.ciurcau@stud.ubbcluj.ro>
2023-05-06 16:03:14 +0300

    add os check test

---
+++ backend/src/main/java/com/example/labsdi/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.example.labsdi.controller;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class AdminController {
+
+    @GetMapping("/admin/test")
+    public ResponseEntity<?> test() throws IOException {
+        if (!SystemUtils.IS_OS_LINUX) {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new Object() {
+            public String getMessage() {return "Server OS is not Linux";}
+        });
+        }
+
+        String shellScript = "echo Hello world";
+        Runtime.getRuntime().exec(shellScript);
+
+        return ResponseEntity.ok(new Object() {
+            public String getMessage() {return "ok";}
+        });
+    }
+}
