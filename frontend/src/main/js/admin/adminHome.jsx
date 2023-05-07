b2a79e131817fede7b93921f6c53c1fa0e0f09c9
Goth11 <leonardo.ciurcau@stud.ubbcluj.ro>
2023-05-07 20:41:46 +0300

    Update adminHome.jsx

--- frontend/src/main/js/admin/adminHome.jsx
+++ frontend/src/main/js/admin/adminHome.jsx
@@ -86,7 +86,7 @@ class AdminHome extends Component {
             .then(response => response.json())
             .then(data => this.setState({userCount: data.count}));
 
-        setTimeout(this.getAllCounts, 10000);
+        setTimeout(this.getAllCounts, 5000);
     }
 
     render() {
@@ -135,7 +135,7 @@ class AdminHome extends Component {
                     </Button>
             </Container>
             <br/>
-            <p>Updates every 10 seconds</p>
+            <p>Updates every 5 seconds</p>
             <TextField InputLabelProps={{shrink: true}} id="outlined-basic" label="Total guitars" variant="filled"
                                InputProps={{readOnly: true,}} value={guitarCount}/>
             &nbsp;
