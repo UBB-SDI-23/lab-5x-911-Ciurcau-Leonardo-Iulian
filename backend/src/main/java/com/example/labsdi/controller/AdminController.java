48c3a003d112fde4e5fc35336a7f1272bfd8f920
Goth11 <leonardo.ciurcau@stud.ubbcluj.ro>
2023-05-07 12:18:10 +0300

    Update AdminController.java

--- backend/src/main/java/com/example/labsdi/controller/AdminController.java
+++ backend/src/main/java/com/example/labsdi/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.example.labsdi.controller;
 import org.apache.commons.lang3.SystemUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,8 @@ import java.io.*;
 @RequestMapping("/api")
 public class AdminController {
 
-    @GetMapping("/admin/test")
-    public ResponseEntity<?> test() {
+    @DeleteMapping("/admin/delete/all")
+    public ResponseEntity<?> deleteAllRecords() {
         if (!SystemUtils.IS_OS_LINUX) {
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new Object() {
             public String getMessage() {return "Server OS is not Linux";}
@@ -22,23 +23,16 @@ public class AdminController {
         }
 
         try {
-            /*String database = "mydb";
-            String user = "dbuser";
-            String password = "1234";
-            String sqlCommand = "\"INSERT INTO user_created(id,user_id) VALUES (987654321,1);\"";
-            String command = String.format(
-                    "expect -c 'spawn psql -d %s -U %s -c %s; "+
-                            "expect \"Password for user %s:\"; send \"%s\\n\"; interact'",
-                    database, user, sqlCommand, user, password);
+            String command = "./deleteAllRecords.exp";
             ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", command);
             pb.redirectErrorStream(true);
             Process process = pb.start();
 
             int exitCode = process.waitFor();
-            System.out.println("Process exited with code " + exitCode);*/
+            System.out.println("Process exited with code " + exitCode);
 
             return ResponseEntity.ok(new Object() {
-                public String getMessage() {return "ok";}
+                public String getMessage() {return "All records deleted successfully";}
             });
         } catch (Exception e) {
             System.out.println(e.getMessage());
