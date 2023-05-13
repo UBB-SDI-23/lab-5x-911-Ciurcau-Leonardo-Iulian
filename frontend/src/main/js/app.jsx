21ddfe00385382cc4eeb45d7d56d133868b3965b
Goth11 <leonardo.ciurcau@stud.ubbcluj.ro>
2023-05-13 13:38:21 +0300

    Update app.jsx

--- frontend/src/main/js/app.jsx
+++ frontend/src/main/js/app.jsx
@@ -45,7 +45,7 @@ const ReactDOM = require('react-dom');
 
 class App extends React.Component {
 
-    static API_URL = 'https://labsdi-shop.mooo.com';
+    static API_URL = 'https://13.49.137.247';
     constructor(props) {
         super(props);
     }
